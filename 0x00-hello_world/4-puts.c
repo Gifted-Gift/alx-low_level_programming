28236ecdbad4ca2253790fa085e5d54695c8037f
“Gifted-Gift” <giftorevaetomu@gmail.com>
2022-10-13 08:40:49 -0700

    T4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <studio.h>
+/**
+ * main - printing
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
