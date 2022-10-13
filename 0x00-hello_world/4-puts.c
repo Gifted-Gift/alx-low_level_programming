0821352de59f70b5a49387915bfe7c036dab68e3
“Gifted-Gift” <giftorevaetomu@gmail.com>
2022-10-13 09:06:25 -0700

    T4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,17 @@
-#include <studio.h>
+#include<stdio.h>
 
 /**
- * main - this prints out text
- * function - main
- * Description: this just prints a line of text i put there
- * Return: the return value should be zero
- */
+* main - this prints out text
+* function - main
+*Description: this just prints a line of text i put there
+* Return: the return value should be zero
+*/
 int main(void) /* return 0 */
+
 {
 	char Mytext[] = "\"Programming is like building a multilingual puzzle";
 
 	puts(Mytext);
-	
+
 	return (0);
 }
