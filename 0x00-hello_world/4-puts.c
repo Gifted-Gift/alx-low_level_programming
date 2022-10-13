e43bd48c1bd456ca4031e4a3b21463614574dd8f
“Gifted-Gift” <giftorevaetomu@gmail.com>
2022-10-13 08:50:11 -0700

    T4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <studio.h>
 /**
- * main - printing
- *
- * Return: Always 0 (Successful)
+ * main - this prints out text
+ * function - main
+ * Description: this just prints a line of text i put there
+ * Return: the return value should be zero
  */
-int main(void)
+int main(void) /* return 0 */
 {
 	char Mytext[] = "\"Programming is like building a multilingual puzzle";
 	puts(Mytext);
