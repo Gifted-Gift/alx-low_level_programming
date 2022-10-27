0d36cb83f6182f90063d39892a23a67968865411
“Gifted-Gift” <giftorevaetomu@gmail.com>
2022-10-27 03:04:21 -0700

    alx-low_level_programming - 0x06-pointers_arrays_strings Tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,43 +1,56 @@
 #include "main.h"
 
-int check_seperators(char c);
+int check_separators(char c);
 
 /**
  * main: cap_string - function to capitalise all words of a string.
  * Description: a function that capitalises all words of a string.
  * Prototype: char *cap_string(char *);
  * Separators of words: space, tabulation,
- * new line, ,, ;, ., !, ?, ''(actually double quote), (, ), {, and }
+ * new line, ,, ;, ., !, ?, ", (, ), {, and }
  * @s: An input string to capitalise letters
  * Return: pointer to s
  */
 
 char *cap_string(char *strg)
 {
-	int ind = 0;
+        int ind = 0;
 
-	while (strg[ind])
-	{
-		while (!(strg[ind] >= 'a' && strg[ind] <= 'z'))
-			ind++;
+        while (s[ind])
+        {
+                if (ind == 0 && (strg[ind] >= 'a' && strg[ind] <= 'z'))
 
-		if (strg[ind - 1] == ' ' ||
-			strg[ind - 1] == '\t' ||
-			strg[ind - 1] == '\n' ||
-			strg[ind - 1] == ',' ||
-			strg[ind - 1] == ';' ||
-			strg[ind - 1] == '.' ||
-			strg[ind - 1] == '!' ||
-			strg[ind - 1] == '?' ||
-			strg[ind - 1] == '"' ||
-			strg[ind - 1] == '(' ||
-			strg[ind - 1] == ')' ||
-			strg[ind - 1] == '{' ||
-			strg[ind - 1] == '}' ||
-			ind == )
-				strg[ind] -=12;
-		ind++;
-	}
+                        strg[ind] -= 32;
 
-	return (strg);
+                if (check_separators(strg[ind]) && (strg[ind + 1] >= 'a' && strg[ind + 1] <= 'z'))
+
+                        strg[ind + 1] -= 32;
+
+                ind++;
+        }
+
+        return (s);
+}
+
+/**
+ * check_separators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
+ * @c: an input character
+ * Return: 1 if separator, 0 otherwise
+ */
+
+int check_separators(char c)
+{
+        int ind = 0;
+
+        char separators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+                '"', '(', ')', '{', '}' };
+
+        for (; ind < 13; ind++)
+        {
+                if (c == separators[ind])
+
+                        return (1);
+        }
+        return (0);
 }
