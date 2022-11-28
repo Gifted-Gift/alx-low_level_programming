be135f5a1119e2f8831f501ed3f63db40627354c
“Gifted-Gift” <giftorevaetomu@gmail.com>
2022-11-28 14:05:05 -0800

    7-get_nodeint.c

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -6,23 +6,20 @@
  * @index: The index of the node
  * Return: A pointer to node at specified index, or NULL if not exist
  */
-
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-		unsigned int i = 0;
+	unsigned int i = 0;
 
-			if (head == NULL)
-						return (NULL);
+	if (head == NULL)
+		return (NULL);
 
-				while (head)
-						{
-									if (i == index)
-													return (head);
-											head = head->next;
-													i++;
-														}
-					return (NULL);
+	while (head)
+	{
+		if (i == index)
+			return (head);
+		head = head->next;
+		i++;
+	}
+	return (NULL);
 }
-
-
 
